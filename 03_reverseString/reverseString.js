77da1ec40247b3c50cb66c43315e7c871577f52b
Craig Rossiter <crossiter0@gmail.com>
2024-05-03 07:05:27 -0500

    Completed Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reversedWord = '';
+    word.split('').forEach(letter => {
+        reversedWord = letter + reversedWord;
+    })
+    return reversedWord;
 };
 
+console.log(reverseString('hello'));
 // Do not edit below this line
 module.exports = reverseString;
