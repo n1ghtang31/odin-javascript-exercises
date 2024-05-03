25a13ea67f006728f517544bee28a22449a56d6b
Craig Rossiter <crossiter0@gmail.com>
2024-05-03 07:47:48 -0500

    Completed Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+array = array.filter(item => !args.includes(item));
 
-};
-
+return array;
+}
+console.log(removeFromArray([1, 2, 3, 4], 3));
 // Do not edit below this line
 module.exports = removeFromArray;
