8a8507b06a3c9f30792fe33dc768af0fc2ef56f5
Craig Rossiter <crossiter0@gmail.com>
2024-05-02 21:17:46 -0500

    Completed Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
 
+const repeatString = function(strings, times) {
+    const stringBefore = strings;
+    if (times === 0) {
+        return '';
+    }
+     if (times < 0) {
+         return 'ERROR';
+     }
+    for (let i = 1; i < times; i++) {
+      
+        strings += stringBefore;
+
+    }
+    return strings;
 };
 
 // Do not edit below this line
